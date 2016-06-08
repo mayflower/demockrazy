2a5404fed2c87160722a6dc0fcab1aef189b9978
Markus Mueller <john.subscriber@markus.institute>
2016-06-08 17:18:13 +0000

    Change urls.py layout

--- vote/urls.py
+++ vote/urls.py
@@ -1,10 +1,15 @@
-from django.conf.urls import url
+from django.conf.urls import include, patterns, url
 
 from . import views
 
+pollpatterns = [
+    url(r'^$', views.poll, name='poll'),
+    url(r'^vote$', views.vote, name='vote'),
+    url(r'^success$', views.success, name='success'),
+    url(r'^manage$', views.manage, name='manage'),
+]
+
 urlpatterns = [
     url(r'^$', views.create, name='create'),
-    url(r'^(?P<poll_identifier>[a-zA-Z]+)/$', views.poll, name='poll'),
-    url(r'^(?P<poll_identifier>[a-zA-Z]+)/vote/$', views.vote, name='vote'),
-    url(r'^(?P<poll_identifier>[a-zA-Z]+)/success/$', views.success, name='success'),
+    url(r'^(?P<poll_identifier>[a-zA-Z]+)/', include(pollpatterns)),
 ]
