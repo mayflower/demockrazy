72d1a2c0bf3de9804cb6a3f238f66ecdfe64da9d
Leo Settele <lsettele@eleven.local>
2016-06-08 15:08:49 +0200

    add dumb model for polls, choices and tokens

--- vote/models.py
+++ vote/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+class Poll(models.Model):
+    title = models.CharField(max_length=200)
+    question_text = models.TextField()
     pub_date = models.DateTimeField('date published')
+    creator_token = models.CharField(max_length=512)
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    choice_text = models.TextField()
     votes = models.IntegerField(default=0)
+
+class Token:
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    token_string = models.CharField(max_length=128)
