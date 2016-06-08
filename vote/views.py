09423e58535e022b7863c9fba86fba1c760573fa
Markus Mueller <john.subscriber@markus.institute>
2016-06-08 14:05:39 +0000

    Add dummy html templates

--- vote/views.py
+++ vote/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Poll
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'vote/index.html')
+
+def list(request):
+    polls = Poll.objects.order_by('-pub_date')
+    context = {
+        'polls': polls
+    }
+    return render(request, 'vote/list.html', context)
