7a3c046b7577a314839dbedd236da3032c84b47a
Markus Mueller <john.subscriber@markus.institute>
2016-06-08 14:24:25 +0000

    Fix model and add all Models to admin interface

--- vote/models.py
+++ vote/models.py
@@ -11,6 +11,8 @@ class Poll(models.Model):
     is_active = models.BooleanField(default=True)
     identifier = models.CharField(max_length=64)
 
+    def __str__(self):
+        return self.title
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
@@ -18,6 +20,6 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
 
-class Token:
+class Token(models.Model):
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
     token_string = models.CharField(max_length=128)
