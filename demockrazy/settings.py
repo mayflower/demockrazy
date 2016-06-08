226a441614a10df68fa879da5ff579fa2507bd09
Leo Settele <lsettele@eleven.local>
2016-06-08 14:24:45 +0200

    fix typo

--- demockrazy/settings.py
+++ demockrazy/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'votes.apps.VoteConfig',
+    'vote.apps.VoteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
