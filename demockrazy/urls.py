b5e5ec3525bcd4d69e38faa3e75e608f6aac76e3
Markus Mueller <john.subscriber@markus.institute>
2016-06-09 22:40:34 +0000

    Add redirect for index

--- demockrazy/urls.py
+++ demockrazy/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url="vote/", permanent=False)),
     url(r'^vote/', include('vote.urls', namespace='vote')),
     url(r'^admin/', admin.site.urls),
 ]
