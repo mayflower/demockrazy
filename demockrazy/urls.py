440dc385f09625a98ce15dffe8d05ae62485ecfb
Markus Mueller <john.subscriber@markus.institute>
2016-06-09 17:46:32 +0000

    Revert "Fix urls"
    This reverts commit f2ea128aff4c65253f6128061be606a9ff4a93b4.

--- demockrazy/urls.py
+++ demockrazy/urls.py
@@ -1,9 +1,7 @@
-from django.conf import settings
 from django.conf.urls import include, url
-from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^vote/', include('vote.urls', namespace='vote')),
     url(r'^admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
