6fb80aad81231b0b8cea29eb72b4c9c2c4653916
Markus Mueller <john.subscriber@markus.institute>
2016-06-09 17:48:26 +0000

    Set debug=True

--- demockrazy/settings.py
+++ demockrazy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%cs8fdyvv=@dbe7g^ltxt!=!c(033*tm$br3x%c%u@1%szg8#_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ "wahlcomputer.mayflower.de" ]
 
