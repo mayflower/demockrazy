4f23db933ac97e84cd3b39f8bca594e6218c3768
Markus Mueller <john.subscriber@markus.institute>
2016-06-09 17:39:44 +0000

    Fix STATICFILES_DIRS

--- demockrazy/settings.py
+++ demockrazy/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
 
 EMAIL_HOST = "mail.mayflower.de"
 EMAIL_PORT = 25
