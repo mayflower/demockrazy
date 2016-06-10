00c5a249881bdc026707c801b7b3877769b2b49e
Tristan Helmich <tristan.helmich@gmail.com>
2016-06-10 14:12:08 +0200

    disable debug mode (again)

--- demockrazy/settings.py
+++ demockrazy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%cs8fdyvv=@dbe7g^ltxt!=!c(033*tm$br3x%c%u@1%szg8#_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ "wahlcomputer.mayflower.de" ]
 
